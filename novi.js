2ec8e97ea2373858fa20df617e8176d5535da90e
babicpetra <petrababiczh@gmail.com>
2016-03-25 18:23:26 +0100

    Initial commit

---
+++ novi.js
@@ -0,0 +1,5 @@
+var tekst="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum sagittis suscipit. <strong >Vestibulum ac mattis orci. Vestibulum posuere laoreet velit ac posuere</strong>. Phasellus ultrices turpis nec condimentum maximus. Aliquam erat volutpat. Praesent ut pellentesque risus. Suspendisse mi nunc, ultrices a porttitor venenatis, consequat ut diam. In hac habitasse platea dictumst. Nulla facilisi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum sagittis suscipit. <strong >Vestibulum ac mattis orci. Vestibulum posuere laoreet velit ac posuere</strong>. Phasellus ultrices turpis nec condimentum maximus. Aliquam erat volutpat. Praesent ut pellentesque risus. Suspendisse mi nunc, ultrices a porttitor venenatis, consequat ut diam. In hac habitasse platea dictumst. Nulla facilisi.";
+
+function myFunction(){
+document.getElementById("paragraf").innerHTML=tekst;
+}
